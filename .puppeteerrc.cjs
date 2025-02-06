2a9a776426395532ba0dfd34f75e4c7dfb39bc67
Anthony Chiu <kingychiu@gmail.com>
2025-02-06 21:44:58 +1100

    Try fixing Puppeteer

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,11 +1,12 @@
 // https://stackoverflow.com/questions/77779601/puppeteer-could-not-find-chrome-ver-119-0-6045-105-error
 // https://github.com/puppeteer/puppeteer/issues/10388
-const { join } = require("path");
+// https://pptr.dev/guides/configuration#changing-the-default-cache-directory
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
